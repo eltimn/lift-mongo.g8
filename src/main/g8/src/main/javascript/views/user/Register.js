a5543f058dc1f31a9bf7e929d224df11bfc2c54b
Joe Barnes <barnesjd@gmail.com>
2013-05-11 16:10:27 -0500

    Put registration screen JS into a module

--- src/main/g8/src/main/javascript/views/user/Register.js
+++ src/main/g8/src/main/javascript/views/user/Register.js
@@ -1,47 +1,56 @@
 App.namespace("views.user");
+App.views.user.Register = (function($) {
+	"use strict";
 
-// Global object for localizable strings per 
-// https://groups.google.com/forum/?fromgroups=#!topic/liftweb/DwUOKgmiV-0
-window.strings = {
-    PasswordsMustMatch: "Passwords must match"
-};
+	// Global object for localizable strings per
+	// https://groups.google.com/forum/?fromgroups=#!topic/liftweb/DwUOKgmiV-0
+	window.strings = {
+		PasswordsMustMatch : "Passwords must match"
+	};
 
-$(document).ready(function() {
-	var confirm = $('.confirm-field');
-	var password = $('.password-field');
-	
-    confirm.change(function() { 
-        checkConfirmError();
-    });
-    password.change(function() { 
-        checkConfirmError();
-    });
-    password.bind('input change keyup', function() {
-    	checkConfirmClear();
-    });
-    confirm.bind('input change keyup', function() {
-    	checkConfirmClear();
-    });
-});
+	$(document).ready(function() {
+		var confirm = $('.confirm-field');
+		var password = $('.password-field');
 
-function checkConfirmError() {
-	var confirm = $('.confirm-field');
-	var password = $('.password-field');
-	
-	if(confirm.val() !== password.val() && confirm.val().length > 0 && password.val().length > 0) {
-		if(confirm.next().children().length == 0) {
-			confirm.next().append('<ul><li class="error">'+window.strings.PasswordsMustMatch+'</li></ul>');
-			confirm.parent().parent().addClass("error")
+		confirm.change(function() {
+			checkConfirmError();
+		});
+		password.change(function() {
+			checkConfirmError();
+		});
+		password.bind('input change keyup', function() {
+			checkConfirmClear();
+		});
+		confirm.bind('input change keyup', function() {
+			checkConfirmClear();
+		});
+	});
+
+	function checkConfirmError() {
+		var confirm = $('.confirm-field');
+		var password = $('.password-field');
+
+		if (confirm.val() !== password.val() && confirm.val().length > 0
+				&& password.val().length > 0) {
+			if (confirm.next().children().length == 0) {
+				confirm.next().append(
+						'<ul><li class="error">'
+								+ window.strings.PasswordsMustMatch
+								+ '</li></ul>');
+				confirm.parent().parent().addClass("error")
+			}
 		}
-	}
-};
+	};
 
-function checkConfirmClear() {
-	var confirm = $('.confirm-field');
-	var password = $('.password-field');
+	function checkConfirmClear() {
+		var confirm = $('.confirm-field');
+		var password = $('.password-field');
+
+		if (confirm.val() === password.val()) {
+			confirm.next().children().remove()
+			confirm.parent().parent().removeClass("error")
+		}
+	};
 	
-	if(confirm.val() === password.val()) {
-		confirm.next().children().remove()
-		confirm.parent().parent().removeClass("error")
-	}
-};
+	return inst;
+}(jQuery));
