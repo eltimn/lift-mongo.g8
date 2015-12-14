1bd18fad0e8ed25ac6c514592510c0ca75ff73c0
Tim Nelson <tim@eltimn.com>
2015-12-14 13:46:20 -0600

    Make BaseScreen a trait

--- src/main/g8/src/main/scala/$package$/snippet/BaseScreen.scala
+++ src/main/g8/src/main/scala/$package$/snippet/BaseScreen.scala
@@ -11,7 +11,6 @@ import net.liftmodules.extras.Bootstrap3Screen
 /*
  * Base all LiftScreens off this. Currently configured to use bootstrap 3.
  */
-abstract class BaseScreen extends Bootstrap3Screen {
+trait BaseScreen extends Bootstrap3Screen {
   override def defaultToAjax_? = true
 }
-
